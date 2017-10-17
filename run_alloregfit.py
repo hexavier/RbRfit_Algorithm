db1af310fc273c9c2b3136e1b4b397c6e6f17ea6
hexavier <hexavier@student.ethz.ch>
2017-10-17 09:59:38 +0200

    Rename module and add more functions
    Functions write_rate_equations and build_priors added.

--- run_alloregfit.py
+++ run_alloregfit.py
@@ -7,15 +7,21 @@ Created on Fri Oct 13 09:06:20 2017
 
 import pandas as pd
 import cobra
-import new_simmer as sim
+import alloregfit as arf
 
 data_dir = "C:/Users/user/polybox/MASTER/THESIS/2_new_simmer_test/"
 model = cobra.io.sbml.create_cobra_model_from_sbml_file(data_dir+"ecoli_core_model.xml")
 #model = pd.read_excel(data_dir+"ecoli_core_model.xls",index_col="METABOLITE")
 fluxes = pd.read_excel(data_dir+"fluxes.xlsx",index_col="name")
+#fluxes_sd = pd.read_excel(data_dir+"fluxes.xlsx",sheetname=1,index_col="name")
 metabolites = pd.read_excel(data_dir+"metabolites.xlsx",index_col="name")
+#metabolites_sd = pd.read_excel(data_dir+"metabolites.xlsx",sheetname=1,index_col="name")
 proteins = pd.read_excel(data_dir+"proteins.xlsx",index_col="name")
 mapping = pd.read_table(data_dir+"ECOLI_83333_idmapping.dat",header=None)
 rxn_id = open(data_dir+'reactions.txt').read().splitlines()
 
-summary = sim.define_reactions(rxn_id,model,fluxes,proteins,metabolites)
+summary = arf.define_reactions(rxn_id,model,fluxes,proteins,metabolites)
+
+for idx in list(summary.index):
+    expr,parameters = arf.write_rate_equations(idx,summary,model)
+    parameters = arf.build_priors(parameters,idx,summary,model)
