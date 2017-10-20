c7ce9a0f5af3e10258706877f2cc23b7a2a12a28
hexavier <hexavier@student.ethz.ch>
2017-10-20 10:58:38 +0200

    Time performance

--- run_alloregfit.py
+++ run_alloregfit.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import cobra
 import alloregfit as arf
+from time import time
 
 data_dir = "C:/Users/user/polybox/MASTER/THESIS/2_new_simmer_test/"
 model = cobra.io.sbml.create_cobra_model_from_sbml_file(data_dir+"ecoli_core_model.xml")
@@ -16,8 +17,14 @@ mapping = pd.read_table(data_dir+"ECOLI_83333_idmapping.dat",header=None)
 rxn_id = open(data_dir+'reactions.txt').read().splitlines()
 reg_coli = pd.read_csv(data_dir+"SMRN.csv",index_col="rxn_id")
 
+s = time()
 summary = arf.define_reactions(rxn_id,model,fluxes,proteins,metabolites)
 candidates = arf.define_candidates(rxn_id,reg_coli,metabolites)
-markov_par = {'freq':20,'nrecord':200,'burn_in':0} # Record once every 20 samples, 200 samples, skip 0 first samples
-
+markov_par = {'freq':10,'nrecord':100,'burn_in':0} # Record once every 20 samples, 200 samples, skip 0 first samples
+e = time()
+print('Load summary and candidates: %fs' % (e-s))
+#%%
+s = time()
 results = arf.fit_reactions(summary,model,markov_par)
+e = time()
+print('Fit reaction MCMC-NNLS: %fs' % (e-s))
