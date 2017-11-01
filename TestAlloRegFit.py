c1dd73da425e5a6eeb922745c8e47f2e2eac709d
hexavier <hexavier@student.ethz.ch>
2017-11-01 16:32:10 +0100

    Update unit test with the changes made in alloregfit

--- TestAlloRegFit.py
+++ TestAlloRegFit.py
@@ -16,7 +16,7 @@ rxn_id = open(data_dir+'reactions.txt').read().splitlines()
 reg_coli = pd.read_csv(data_dir+"SMRN.csv",index_col="rxn_id")
 binding_site = [[['fum_c', 'mal_L_c']], [['6pgc_c', 'ru5p_D_c'], ['nadp_c', 'nadph_c'], ['co2_c']], [['mal_L_c', 'oaa_c'], ['nad_c', 'nadh_c']], [['atp_c', 'adp_c'], ['f6p_c', 'fdp_c']], [['g6p_c', 'f6p_c']], [['atp_c', 'amp_c'], ['pyr_c', 'pep_c']], [['adp_c', 'atp_c'], ['pep_c', 'pyr_c']], [['r5p_c', 'ru5p_D_c']]]
 
-summary,bools = arf.define_reactions(rxn_id,model,binding_site,fluxes,proteins,metabolites)
+summary,bools = arf.define_reactions(rxn_id,model,fluxes,proteins,metabolites,binding_site)
 candidates = arf.define_candidates(rxn_id,reg_coli,metabolites,bools)
 markov_par = {'freq':10,'nrecord':20,'burn_in':0}
 
@@ -29,13 +29,13 @@ class TestAlloRegFit(unittest.TestCase):
     def test_size(self):
         self.assertEqual(summary.shape, (8, 7))
         self.assertEqual(candidates.shape, (8, 4))
-        self.assertEqual(results.shape, (22, 8))
+        self.assertEqual(results.shape, (22, 10))
         
     def test_results(self):
         mean = np.mean(results['best_lik'])
-        self.assertEqual(mean, -20.469714179933025)
-        self.assertTrue(results.index.values[0]==4.0)
-        self.assertTrue(results.index.values[-1]==0.0)
+        self.assertEqual(mean, -18.662868682058285)
+        self.assertTrue(results['idx'][0]==3.0)
+        self.assertTrue(results['idx'][21]==7.0)
 
 if __name__ == '__main__':
     unittest.main()
