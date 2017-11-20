5a1c074eb96af5b087c1cf75d82727ea839ca2bc
hexavier <hexavier@student.ethz.ch>
2017-11-20 10:30:07 +0100

    Revert "New version"
    This reverts commit e4013f03c64cb4d4e874cd7115b9be6cd90b3095.

--- run_alloregfit_Karl.py
+++ run_alloregfit_Karl.py
@@ -6,11 +6,10 @@ import alloregfit as arf
 from time import time
 
 cond = ['glc-NCM3722_1','glc-NQ1243_3','glc-NQ1243_4','glc-NQ1243_5','glu-NCM3722_9','glu-NQ393_11','glu-NQ393_12','glu-NQ393_13','glu-NQ393_14']
-data_dir = 'Y:/users/Xavier/3_Karl_data/'
+data_dir = "C:/Users/user/polybox/MASTER/THESIS/3_Karl_data/"
 model = cobra.io.load_matlab_model(data_dir+"iJO1366.mat")
 fluxes = pd.read_excel(data_dir+"fluxes_C13.xlsx",index_col="name")[cond]
 metabolites = pd.read_excel(data_dir+"merged_metabolites.xlsx",index_col="name")[cond]
-metabolites_sd = pd.read_excel(data_dir+"merged_metabolites_sd.xlsx",index_col="name")[cond]
 proteins = pd.read_excel(data_dir+"proteome.xlsx",index_col="name")[cond]
 mapping = pd.read_table(data_dir+"ECOLI_83333_idmapping.dat",header=None)
 rxn_id = open(data_dir+'reactions.txt').read().splitlines()
@@ -19,16 +18,16 @@ reg_coli = pd.read_csv(data_dir+"SMRN.csv",index_col="rxn_id")
 s = time()
 #binding_site = arf.get_binding_sites(rxn_id,model)
 #binding_site = [[['dhap_c', 'fdp_c', 'g3p_c']], [['fum_c', 'mal__L_c']], [['6pgl_c', 'g6p_c'], ['nadp_c', 'nadph_c']], [['6pgc_c', 'ru5p__D_c'], ['nadp_c', 'nadph_c'], ['co2_c']], [['mal__L_c', 'oaa_c'], ['nad_c', 'nadh_c']], [['nad_c', 'nadh_c'], ['nadp_c', 'nadph_c']], [['adp_c', 'atp_c'], ['f6p_c', 'fdp_c']], [['f6p_c', 'g6p_c']], [['adp_c', 'atp_c'], ['pep_c', 'pyr_c']], [['g3p_c', 'xu5p__D_c'], ['r5p_c', 's7p_c']], [['dhap_c', 'g3p_c']]]
-summary = arf.define_reactions(rxn_id,model,fluxes,proteins,metabolites,metab_sd=metabolites_sd)
-candidates = arf.define_candidates(rxn_id,reg_coli,metabolites,metabolites_sd)
-markov_par = {'freq':1,'nrecord':2,'burn_in':0} # Record once every 20 samples, 200 samples, skip 0 first samples
+summary,bools = arf.define_reactions(rxn_id,model,fluxes,proteins,metabolites)
+candidates = arf.define_candidates(rxn_id,reg_coli,metabolites,bools)
+markov_par = {'freq':20,'nrecord':200,'burn_in':0} # Record once every 20 samples, 200 samples, skip 0 first samples
 e = time()
 print('Load summary and candidates: %fs' % (e-s))
 
 #%% Run fit with regulators
 s = time()
-results = arf.fit_reactions(summary,model,markov_par,metabolites,metabolites_sd)#,candidates,maxreg=1,coop=True)
+results = arf.fit_reactions(summary,model,markov_par,candidates)#,candidates,maxreg=1,coop=True)
 e = time()
 print('Fit reaction MCMC-NNLS: %fs' % (e-s))
 
-#results.to_pickle(data_dir+'fit_1_candidates.pickle')
+results.to_pickle(data_dir+'fit_1_candidates.pickle')
